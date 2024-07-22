21587af77200d3ac92be31bfc3880b7c559eb17f
MoodyEG <moodyeg2011@yahoo.com>
2024-07-22 16:44:26 +0300

    Task #1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    const task2 = false;
+    if (task === task2) {
+      task = task2;
+    }
+  }
+
+  return [task, task2];
+}

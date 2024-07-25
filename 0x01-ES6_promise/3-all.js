0ee5eaf02c0dbb33fc87766a84531696f74538be
MoodyEG <moodyeg2011@yahoo.com>
2024-07-25 03:43:46 +0300

    Fixing 3, again

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].lastName);
+    .then((r) => {
+      console.log(`${r[0].body} ${r[1].firstName} ${r[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }

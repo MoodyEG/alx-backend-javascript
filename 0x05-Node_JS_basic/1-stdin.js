02ac0645cdf1be243305ecab0f2d198d4c268632
MoodyEG <moodyeg2011@yahoo.com>
2024-09-17 15:29:53 +0300

    Fixes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input}\n`);
-  rl.close();
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
 });
 
-rl.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit(0);
 });
--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,13 +4,13 @@ module.exports = function countStudents(path) {
   return fs.readFile(path, { encoding: 'utf8' })
     .then((data) => {
       const lines = data.split('\n').filter((line, index) => index !== 0 && line !== '').map((line) => line.split(','));
-      const students = lines.map((line) => line[0]);
+      const students = lines.map((line) => ({ name: line[0], field: line[3] }));
       const fields = Array.from(new Set(lines.map((line) => line[3])));
       console.log(`Number of students: ${students.length}`);
       fields.forEach((field) => {
         const fieldStudents = Array.from(new Set(students.filter(
-          (student) => lines.find((line) => line[3] === field && line[0] === student),
-        )));
+          (student) => student.field === field,
+        ).map((student) => student.name)));
         console.log(`Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`);
       });
     })

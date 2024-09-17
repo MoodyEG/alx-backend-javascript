2b2c2f7f92afa1eb2a7bccd1412da1e28e222662
MoodyEG <moodyeg2011@yahoo.com>
2024-09-17 20:46:43 +0300

    Testing 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,30 +1,30 @@
 const express = require('express');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const app = express();
 const hostname = 'localhost';
 const port = 1245;
 
-async function countStudents(path) {
-  return fs.readFile(path, { encoding: 'utf8' })
-    .then((data) => {
-      const lines = data.split('\n').filter((line, index) => index !== 0 && line !== '').map((line) => line.split(','));
-      const students = lines.map((line) => ({ name: line[0], field: line[3] }));
-      const fields = Array.from(new Set(lines.map((line) => line[3])));
-      let output = `Number of students: ${students.length}\n`;
-      fields.forEach((field) => {
-        const fieldStudents = Array.from(new Set(students.filter(
-          (student) => student.field === field,
-        ).map((student) => student.name)));
-        output += `Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}\n`;
-      });
-      return output;
-    })
-    .catch((error) => {
-      if (error) {
-        throw new Error('Cannot load the database');
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').filter((line, index) => index !== 0 && line !== '').map((line) => line.split(','));
+        const students = lines.map((line) => ({ name: line[0], field: line[3] }));
+        const fields = Array.from(new Set(lines.map((line) => line[3])));
+        let output = `Number of students: ${students.length}\n`;
+        fields.forEach((field) => {
+          const fieldStudents = Array.from(new Set(students.filter(
+            (student) => student.field === field,
+          ).map((student) => student.name)));
+          output += `Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}\n`;
+        });
+        resolve(output);
       }
     });
+  });
 }
 
 app.get('/', (req, res) => {
@@ -32,8 +32,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', async (req, res) => {
-  const output = await countStudents(process.argv[2]);
-  res.send(output);
+  try {
+    const output = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${output}`);
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 app.listen(port, hostname, () => {

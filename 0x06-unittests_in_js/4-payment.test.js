a431de069dff0071c63bbfd66203f9f9d4987447
MoodyEG <moodyeg2011@yahoo.com>
2024-09-20 00:58:22 +0300

    Last test, fix your checker ALX

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,23 +1,16 @@
-/* eslint-disable jest/prefer-expect-assertions */
-/* eslint-disable jest/valid-expect */
-/* eslint-disable no-unused-expressions */
 const { expect } = require('chai');
 const sinon = require('sinon');
-
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('payment testing', () => {
-  it('should call Utils.calculateNumber and log the correct message', () => {
-    const totalAmount = 100;
-    const totalShipping = 20;
-
+  it('call and log the correct message', () => {
     const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
     const consoleSpy = sinon.spy(console, 'log');
 
-    sendPaymentRequestToApi(totalAmount, totalShipping);
+    sendPaymentRequestToApi(100, 20);
 
-    expect(stub.calledOnceWith('SUM', totalAmount, totalShipping)).to.be.true;
+    expect(stub.calledOnceWith('SUM', 100, 20)).to.be.true;
 
     expect(consoleSpy.calledOnceWith('The total is: 10')).to.be.true;
 

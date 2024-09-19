0d207cd9c38ec1d65c0d7b36991375e1300ba666
MoodyEG <moodyeg2011@yahoo.com>
2024-09-19 16:03:39 +0300

    Testing 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,36 @@
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+  it('two zeros', () => {
+    assert.equal(calculateNumber(0, 0), 0);
+  });
+
+  it('two int', () => {
+    assert.equal(calculateNumber(1, 2), 3);
+  });
+
+  it('one int one round float', () => {
+    assert.equal(calculateNumber(1.5, 2), 4);
+  });
+
+  it('one int one non round float', () => {
+    assert.equal(calculateNumber(1.3, 2), 3);
+  });
+
+  it('round float', () => {
+    assert.equal(calculateNumber(1.5, 2.5), 5);
+  });
+
+  it('not exact', () => {
+    assert.notEqual(calculateNumber(1.5, 2.5), 4);
+  });
+
+  it('negative numbers', () => {
+    assert.equal(calculateNumber(-1.5, -2.5), -3);
+  });
+
+  it('negative and positive numbers', () => {
+    assert.equal(calculateNumber(-1.5, 2.5), 2);
+  });
+});

5fd0409b490e99649943899464fe334c9149ddbc
MoodyEG <moodyeg2011@yahoo.com>
2024-09-19 21:51:49 +0300

    Testing 4.2

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,31 +4,17 @@ const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should use Utils.calculateNumber and log the result', () => {
+  it('using Stub', () => {
     const daStub = sinon.stub(Utils, 'calculateNumber');
     daStub.returns(10);
     const logSpy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
-    expect(daStub.calledWith('SUM', 100, 20)).to.be.true;
-    expect(logSpy.calledWith('The total is: 10')).to.be.true;
+    expect(daStub.calledOnceWith('SUM', 100, 20)).to.be.true;
+    expect(logSpy.calledOnceWith('The total is: 10')).to.be.true;
 
     daStub.restore();
     logSpy.restore();
-  });
-
-  it('should throw an error if Utils.calculateNumber throws an error', (done) => {
-    const stub = sinon.stub(Utils, 'calculateNumber');
-    stub.throws(new Error('Fake error'));
-
-    try {
-      sendPaymentRequestToApi(100, 20);
-    } catch (error) {
-      expect(error.message).to.equal('Fake error');
-    }
-
-    stub.restore();
-    done();
   });
 });

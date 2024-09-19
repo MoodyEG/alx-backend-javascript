93f9cc3ce0482471591927e1cc7dab19c46fb067
MoodyEG <moodyeg2011@yahoo.com>
2024-09-19 23:40:32 +0300

    Tesing 4.6

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,29 @@
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable no-unused-expressions */
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('payment testing', () => {
+  it('should call Utils.calculateNumber and log the correct message', () => {
+    const totalAmount = 100;
+    const totalShipping = 20;
+
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+
+    expect(stub.calledOnce).to.be.true;
+    expect(stub.calledWith('SUM', totalAmount, totalShipping)).to.be.true;
+
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+
+    stub.restore();
+    consoleSpy.restore();
+  });
+});

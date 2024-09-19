4a7a8dfd58f041158d716cefe8830f3f6a3bbb8a
MoodyEG <moodyeg2011@yahoo.com>
2024-09-19 21:54:22 +0300

    Testing 4.2.2

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
-const expect = require('chai').expect;
+const expect = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {

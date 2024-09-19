d3a8026cb392a9d7b4d84698fd939f56710f74e7
MoodyEG <moodyeg2011@yahoo.com>
2024-09-19 19:08:36 +0300

    Testing 3.2

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,18 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
 const expect = require('chai').expect
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('should use Utils.calculateNumber and log the result', () => {
     const spy = sinon.spy(Utils, 'calculateNumber');
     const logSpy = sinon.spy(console, 'log');
-    const sendPaymentRequestToApi = require('./3-payment');
+
     sendPaymentRequestToApi(100, 20);
+    
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
     expect(logSpy.calledWith('The total is: 120')).to.be.true;
+    
     spy.restore();
     logSpy.restore();
   });
